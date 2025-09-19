8ef238573d6256ed3aefa6d25d4b835fe772df45
Rimskij Papa <r@bzz.bz>
2025-09-19 20:03:38 +0200

    Initial Commit

---
+++ NtfyMenuBar/NtfyMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NtfyMenuBarApp.swift
+//  NtfyMenuBar
+//
+//  Created by Rimskij Papa on 19/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct NtfyMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
